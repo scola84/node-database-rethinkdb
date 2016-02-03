5632e779a31e42f2836ac062bfcc9b38643e7563
M.C. van den Boogaart <scola@kvdb.name>
2016-02-03 14:06:43 +0100

    Do not manage queries

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const Database = require('./lib/database.js');
 
 class Module extends DI.Module {
   configure() {
-    this.inject(Database)
-      .insertArgument(1, this.value(rethinkdb));
+    this.inject(Database).with(
+      this.value(rethinkdb)
+    );
   }
 }
 
